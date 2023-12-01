1ca187cf0b8c126ac41c230a06dc9382e2bb22a3
oskul2021 <albertlionelmalang@gmail.com>
2023-12-01 10:40:26 +0100

    first commit

---
+++ src/main/java/com/bandesoft/medigorx/MedigorxApplication.java
@@ -0,0 +1,13 @@
+package com.bandesoft.medigorx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedigorxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedigorxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bandesoft/medigorx/MedigorxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandesoft.medigorx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedigorxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
